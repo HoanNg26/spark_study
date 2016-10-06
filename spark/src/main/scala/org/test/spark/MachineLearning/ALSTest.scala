6b16b7fe49941d0aa52f40ba39516b6b4541a2ac
Hoan Ng <hoan.khan.0110@gmail.com>
2016-10-07 04:39:00 +0700

    ffff

--- spark/src/main/scala/org/test/spark/MachineLearning/ALSTest.scala
+++ spark/src/main/scala/org/test/spark/MachineLearning/ALSTest.scala
@@ -57,7 +57,7 @@ object ALSTest {
 
     // Save and load model
     model.save(sc, fileInput + ".model")
-
+//
     //    val sameModel = MatrixFactorizationModel.load(sc, "target/tmp/myCollaborativeFilter")
 
   }
